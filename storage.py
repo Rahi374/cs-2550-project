97df52ed6cb05a5bed4b51a1eb826d4f44255dca
--- <shx26@pitt.edu>
2020-03-17 04:42:41 +0800

    completed test scripts and methods except rm_tbl and rm_blk

--- storage.py
+++ storage.py
@@ -34,7 +34,8 @@ class Storage():
 		os.mkdir(path)
 		self.rm_dir_readonly(path)
 
-	def delete_tbl(self, tbl_name: str, cascade = True):
+	def rm_tbl(self, tbl_name: str, cascade = True):
+		#TODO: implement cascade, create test cases
 		os.rmdir(self.mnt_path + tbl_name + '/')
 		
 	def rm_dir_readonly(self, path: str):
@@ -44,7 +45,6 @@ class Storage():
 		os.chmod(path, 0o666)
 
 	def read_blk(self, tbl_name: str, blk_id: int):
-
 		f_path = self.mnt_path + tbl_name + '/' + 'block_' + str(blk_id) + '.img'
 
 		if not os.path.exists(self.mnt_path + tbl_name):
@@ -71,6 +71,10 @@ class Storage():
 			f.write(blk)
 			f.close()
 			return 0
+
+	def rm_blk(self, tbl_name: str, blk_id: int):
+		#TODO: implement
+		return 0
 		
 		
 	
@@ -95,37 +99,47 @@ if __name__ == '__main__':
 		for x in range(5): sto.create_tbl('x_' + str(x))
 		sto.create_tbl('x_1/1_new')
 
-
-		#test write blk + create blk
+		#test write blk + create blk, read blk (too lazy to test read and write separately)
 		sto.write_blk('x_3', 23, 'datadatadatadatadatadatadatadata')
 		sto.write_blk('x_4', 1908, 'whatwhatwhatwhatwhatwhatwhatwhat')
+		check(sto.read_blk('x_3', 23), 'datadatadatadatadatadatadatadata')
+		check(sto.read_blk('x_4', 1908), 'whatwhatwhatwhatwhatwhatwhatwhat')
 
-		#test overwrite blk
+		#test overwrite blk, read blk 
 		sto.write_blk('x_3', 23, '||||||||||||||||||||||||||||||||')
+		check(sto.read_blk('x_3', 23), '||||||||||||||||||||||||||||||||')
 
-
-		#test invalid blk size
+		#test write with invalid blk size
 		try:
 			sto.write_blk('x_4', 10, 'testtesttesttesttest')
 		except Exception as e:
 			check(e, 'invalid block size')
 
-		#test table does not exist
+		#test write with table does not exist
 		try:
 			sto.write_blk('yy', 10, 'testtesttesttesttesttesttesttest')
 		except Exception as e:
 			check(e, 'table does not exist')
 
+		#test read with table does not exist
+		try:
+			sto.read_blk('bb', 123)
+		except Exception as e:
+			check(e, 'table does not exist')
+
+		#test read with block does not exist
+		try:
+			sto.read_blk('x_3', 123)
+		except Exception as e:
+			check(e, 'block does not exist')
+
+		
+
 		
 
 		
 
 		
-		
-
-		# sto.delete_tbl('x_1/1_new')
-		# for x in range(5): sto.delete_tbl('x_' + str(x))
-
 		
 
 
--- storage_test.sh
+++ storage_test.sh
@@ -1,4 +1,6 @@
+#create mount point
 mkdir storage
 rm -rf storage/*
+#run test
 python3 storage.py test
 
