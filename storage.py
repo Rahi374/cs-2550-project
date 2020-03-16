a97a2c3feb6f67dfe51edbc84781957b289909c1
--- <shx26@pitt.edu>
2020-03-17 04:32:12 +0800

    storage:finished all class methods, started unit tests

--- storage.py
+++ storage.py
@@ -1,3 +1,133 @@
-#storage.py  
-class Slotted_Page():
-    def __init__():
+'''
+storage.py  
+
+'''
+# from core import ORG
+from enum import Enum
+import os, shutil, stat, sys
+
+class ORG(Enum):
+	SEQ = 1
+	LSM = 2
+
+class Storage():
+	file_org = None
+	org_str = None
+	blk_size = None
+	mnt_path = None
+
+	def __init__(self, org: ORG, blk_size: int, mnt: str = 'storage/'):
+		#mount point 'storage/'
+		self.file_org = org
+		self.org_str = 'SEQ' if org == ORG.SEQ else 'LSM'
+		self.blk_size = blk_size
+		self.mnt_path = mnt 
+		
+		#2nd mount point
+		add_mnt = 'storage_' + str(blk_size) + '_' + self.org_str
+		self.create_tbl(add_mnt)
+		self.mnt_path += add_mnt + '/'
+		
+
+	def create_tbl(self, tbl_name: str):
+		path = self.mnt_path + tbl_name + '/'
+		os.mkdir(path)
+		self.rm_dir_readonly(path)
+
+	def delete_tbl(self, tbl_name: str, cascade = True):
+		os.rmdir(self.mnt_path + tbl_name + '/')
+		
+	def rm_dir_readonly(self, path: str):
+		os.chmod(path, 0o777)
+
+	def rm_file_readonly(self, path: str):
+		os.chmod(path, 0o666)
+
+	def read_blk(self, tbl_name: str, blk_id: int):
+
+		f_path = self.mnt_path + tbl_name + '/' + 'block_' + str(blk_id) + '.img'
+
+		if not os.path.exists(self.mnt_path + tbl_name):
+			raise Exception('table does not exist')
+		elif not os.path.exists(f_path):
+			raise Exception('block does not exist')
+		else:
+			f = open(f_path, 'r')
+			blk = f.read()
+			f.close()
+			return blk
+			
+		
+	def write_blk(self, tbl_name: str, blk_id: int, blk):
+
+		if not len(blk) == self.blk_size:
+			raise Exception('invalid block size')
+
+		f_path = self.mnt_path + tbl_name + '/' + 'block_' + str(blk_id) + '.img'
+		if not os.path.exists(self.mnt_path + tbl_name):
+			raise Exception('table does not exist')
+		else:
+			f = open(f_path, 'w+')
+			f.write(blk)
+			f.close()
+			return 0
+		
+		
+	
+		
+
+if __name__ == '__main__':
+
+	def check(e, e_test):
+		if not str(e) == str(e_test):
+			print('\033[91m test case failed \033[0m')
+			raise Exception('\033[91m\nexpected: ' + str(e_test) + '\nobserved: ' + str(e) + '\n\033[0m')
+		else:
+			print('\33[32m test case passed \033[0m')
+
+	if len(sys.argv) == 2 and sys.argv[1] == 'test':
+
+		#test constructor
+		sto = Storage(ORG.LSM, 32)
+		check(sto.org_str + ' ' + str(sto.blk_size) + ' ' + sto.mnt_path, 'LSM 32 storage/storage_32_LSM/')
+
+		#test create tables
+		for x in range(5): sto.create_tbl('x_' + str(x))
+		sto.create_tbl('x_1/1_new')
+
+
+		#test write blk + create blk
+		sto.write_blk('x_3', 23, 'datadatadatadatadatadatadatadata')
+		sto.write_blk('x_4', 1908, 'whatwhatwhatwhatwhatwhatwhatwhat')
+
+		#test overwrite blk
+		sto.write_blk('x_3', 23, '||||||||||||||||||||||||||||||||')
+
+
+		#test invalid blk size
+		try:
+			sto.write_blk('x_4', 10, 'testtesttesttesttest')
+		except Exception as e:
+			check(e, 'invalid block size')
+
+		#test table does not exist
+		try:
+			sto.write_blk('yy', 10, 'testtesttesttesttesttesttesttest')
+		except Exception as e:
+			check(e, 'table does not exist')
+
+		
+
+		
+
+		
+		
+
+		# sto.delete_tbl('x_1/1_new')
+		# for x in range(5): sto.delete_tbl('x_' + str(x))
+
+		
+
+
+
+
