d5bea71039129b758430dbae18595f0acfb3d9a8
Paul Elder <paul.elder@pitt.edu>
2020-03-19 00:50:16 -0400

    common: add helpers for padding bytearrays and BitArrays

--- common.py
+++ common.py
@@ -1,3 +1,4 @@
+from bitstring import BitArray
 from enum import Enum
 
 RECORD_SIZE = 32
@@ -11,3 +12,17 @@ class ACTION(Enum):
     RETRIEVE_BY_AREA_CODE = 2
     WRITE_RECORD = 3
     DELETE_TABLE = 4
+
+def pad_bits(ba: BitArray, target_len: int):
+    l = len(ba)
+    if l % 8 == 0:
+        return ba
+    target_len *= 8
+    amount_to_pad = target_len - l
+    return BitArray(amount_to_pad) + ba
+
+def pad_ba(ba: bytearray, length: int):
+    l = len(ba)
+    for i in range(l, length):
+        ba.append(0)
+    return ba
