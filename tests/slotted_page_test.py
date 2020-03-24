14f0bba51633f89fce734b4e76279e9f6d385735
Paul Elder <paul.elder@pitt.edu>
2020-03-23 20:24:18 -0400

    tests: slotted_page: test loading, insertion, and deletion

--- tests/slotted_page_test.py
+++ tests/slotted_page_test.py
@@ -8,6 +8,18 @@ cname2 = "alice"
 phone1 = "1234567890"
 phone2 = "4352769867"
 
+record1 = Record(0, cname1, phone1)
+record2 = Record(1, cname2, phone2)
+
+test_record = Record(5, "abcdefgh", "012-345-6789")
+test_ba = bytearray([
+    5, 0, 0, 0,
+    97, 98, 99, 100, 101, 102, 103, 104, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 49, 50, 45, 51, 52, 53, 45, 54, 55, 56, 57,
+    5, 0, 0, 0,
+    97, 98, 99, 100, 101, 102, 103, 104, 0, 0, 0, 0, 0, 0, 0, 0,
+    48, 49, 50, 45, 51, 52, 53, 45, 54, 55, 0, 0])
+
 class TestSlottedPage(unittest.TestCase):
     def test_regular(self):
         r1 = Record(0, cname1, phone1)
@@ -47,6 +59,52 @@ class TestSlottedPage(unittest.TestCase):
 
         ba = sp.to_bytearray()
         self.assertEqual(len(ba), 66)
+
+    def test_conversion(self):
+        sp = SlottedPage(64)
+
+        sp[0] = test_record
+
+        sp2 = SlottedPage(64, test_ba)
+
+        self.assertEqual(sp[0], sp2[0])
+
+    def test_insert(self):
+        sp = SlottedPage(66)
+        sp[1] = Record(0, cname1, phone1)
+        sp.insert(test_record)
+
+        self.assertEqual(sp[0], test_record)
+
+        sp = SlottedPage(66)
+        sp[0] = Record(0, cname1, phone1)
+        sp.insert(test_record)
+
+        self.assertEqual(sp[1], test_record)
+
+    def test_delete(self):
+        sp = SlottedPage(66)
+        sp.insert(record1)
+        sp.insert(record2)
+
+        sp.delete(record1)
+        self.assertIsNone(sp[0])
+        self.assertEqual(sp[1], record2)
+
+        sp = SlottedPage(66)
+        sp.insert(record1)
+        sp.insert(record2)
+
+        sp.delete(record2)
+        self.assertIsNone(sp[1])
+        self.assertEqual(sp[0], record1)
+
+        sp = SlottedPage(66)
+        sp[1] = record2
+
+        sp.delete(record2)
+        self.assertIsNone(sp[0])
+        self.assertIsNone(sp[1])
 
 if __name__ == '__main__':
     unittest.main()
