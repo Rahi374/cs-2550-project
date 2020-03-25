77aa1c934c26335211d50961bf75411533abbba7
Paul Elder <paul.elder@pitt.edu>
2020-03-24 23:02:13 -0400

    core: fix calls missing self

--- core.py
+++ core.py
@@ -1,58 +1,76 @@
 from common import *
 from enum import Enum
+from inst import Instruction
+from mem_seq import MemSeq
+from record import Record
+from storage import Storage
 
 class Core():
 
     def __init__(self, disk_org: ORG, mem_size: int, block_size: int):
-        self.disk = create_storage(block_size, disk_org)
-        self.mem = create_memory(disk, mem_size, block_size, disk_org)
+        self.disk = self.create_storage(block_size, disk_org)
+        self.mem = self.create_memory(mem_size, block_size, self.disk, disk_org)
 
     def create_storage(self, block_size: int, disk_org: ORG):
         if disk_org == ORG.SEQ:
-            return Storage(block_size)
+            return Storage(disk_org, block_size)
         elif disk_org == ORG.LSM:
-            return Storage(block_size)
+            return Storage(disk_org, block_size)
 
-    def create_memory(self, disk, mem_size: int, block_size: int, disk_org: ORG):
+    def create_memory(self, mem_size: int, block_size: int, disk: Storage, disk_org: ORG):
         if disk_org == ORG.SEQ:
-            return Mem(disk, mem_size, block_size)
+            return MemSeq(mem_size, block_size, disk, disk_org)
         elif disk_org == ORG.LSM:
             return Mem(disk, mem_size, block_size)
 
 
     def run(self, insts: list):
         for inst in insts:
-            self.exec_inst(inst)
+            print(f"Executing: {str(inst)}")
+            try:
+                ret = self.exec_inst(inst)
+            except Exception as e:
+                print(e)
+            print(f"Returned: {ret}")
+        self.mem.print_cache()
+        self.mem.flush()
 
     def exec_inst(self, inst: Instruction):
         if inst.action == ACTION.RETRIEVE_BY_ID:
-            self.read_id(inst.table_name, inst.record_id)
+            return self.read_id(inst.table_name, inst.record_id)
 
-        elif inst.action == ACTION.RETRIEVE_BY_AREA_CODE:
-            self.read_area_code(inst.table_name, inst.record_id)
+        if inst.action == ACTION.RETRIEVE_BY_AREA_CODE:
+            return self.read_area_code(inst.table_name, inst.record_id)
 
-        elif inst.action == ACTION.WRITE_RECORD:
-            self.write(inst.table_name, inst.tuple_data)
+        if inst.action == ACTION.WRITE_RECORD:
+            return self.write(inst.table_name, inst.tuple_data)
 
-        elif inst.action == ACTION.DELETE_TABLE:
-            self.delete(inst.table_name)
+        if inst.action == ACTION.DELETE_RECORD:
+            return self.delete_record(inst.table_name, inst.record_id)
+
+        if inst.action == ACTION.DELETE_TABLE:
+            return self.delete_table(inst.table_name)
 
     def read_id(self, table: str, rec_id: int):
-        return mem.retrieve_rec(table=table, rec_id=rec_id, field="id", is_primary=True)
+        return self.mem.retrieve_rec(table=table, rec_id=rec_id, field_name="id", is_primary=True)
 
     def read_area_code(self, table: str, area_code: int):
-        return mem.retrieve_rec(table=table, rec_id=area_code, field="area_code", is_primary=False)
+        return self.mem.retrieve_rec(table=table, rec_id=area_code, field_name="area_code", is_primary=False)
 
-    def write(self, table: str, record: tuple):
+    def write(self, table: str, record: Record):
         if not self.table_exists(table):
             self.create_table(table)
-        mem.write_rec(table, record)
+        self.mem.write_rec(table, record)
 
-    def delete(self, table: str):
-        disk.delete_table(table)
+    def delete_record(self, table: str, rec_id: int):
+        self.mem.delete_rec(table, rec_id)
+
+    def delete_table(self, table: str):
+        self.mem.delete_table_in_mem(table)
+        self.disk.delete_table(table)
 
     def create_table(self, table: str):
-        disk.create_table(table)
+        self.disk.create_table(table)
 
     def table_exists(self, table: str):
-        disk.table_exists(table)
+        return self.disk.table_exists(table)
