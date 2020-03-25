400ff4c3bfb4221cc4cb5305d06f0e3f777ffbba
Paul Elder <paul.elder@pitt.edu>
2020-03-24 22:59:09 -0400

    parser: fix record splitting and id data type

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
-import ACTION  
+from common import ACTION
+from inst import Instruction
+from record import Record
 import sys
+
 class Parser():
     @staticmethod
     def parse(inst_file):
@@ -8,22 +11,25 @@ class Parser():
         lines = f.readlines()
         line_num = 1
         for line in lines:
-            split_line - line.split()
+            split_line = line.split()
             action = split_line[0]
             table_name = split_line[1]
-            data = split_line[2]
+            data = split_line[2] if len(split_line) > 2 else None
             inst = None
             if action == 'R':
-                inst = Instruction(ACTION.RETRIEVE_BY_ID, table_name, data)
+                inst = Instruction(ACTION.RETRIEVE_BY_ID, table_name, int(data))
             elif action == 'M':
-                inst = Instruction(ACTION.RETRIEVE_BY_AREA_CODE, table_name, data)
+                inst = Instruction(ACTION.RETRIEVE_BY_AREA_CODE, table_name, int(data))
             elif action == 'W':
-                inst = Instruction(ACTION.WRITE_RECORD, table_name, data)
+                data = " ".join(split_line[2:]).strip('()').split(",")
+                inst = Instruction(ACTION.WRITE_RECORD, table_name, Record(int(data[0]), data[1].strip(), data[2].strip()))
+            elif action == 'E':
+                inst = Instruction(ACTION.DELETE_RECORD, table_name, int(data))
             elif action == 'D':
-                inst = Instruction(ACTION.DELETE_TABLE, table_name, data)
+                inst = Instruction(ACTION.DELETE_TABLE, table_name)
             else:
                 print("Malformed Input File on Line "+line_num+": "+line)
-              sys.exit()
+                sys.exit()
             instructions.append(inst)
             line_num += 1
         return instructions
