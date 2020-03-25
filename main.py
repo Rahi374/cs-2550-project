1b61069dde78016bd4c93e5d241e2bef637ddc34
Paul Elder <paul.elder@pitt.edu>
2020-03-24 22:57:13 -0400

    main: fix so that it runs, and add test instruction sequence

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # main.py
 from common import *
+from core import Core
+from inst import Instruction
+from parser import Parser
+from record import Record
 import sys
 
 def print_usage():
@@ -18,16 +22,27 @@ disk_org = (ORG.SEQ if sys.argv[2] == "SEQ" else ORG.LSM)
 mem_size = sys.argv[3]
 block_size = sys.argv[4]
 
-#insts = parser.parse(instr_file)
+real_insts = Parser.parse(instr_file)
 insts = [
         Instruction(ACTION.RETRIEVE_BY_ID,        "X", 13),
         Instruction(ACTION.RETRIEVE_BY_ID,        "Y", 7),
-        Instruction(ACTION.WRITE_RECORD,          "Y", (5, "John", "412-111-2222")),
+        Instruction(ACTION.WRITE_RECORD,          "Y", Record(5, "John", "412-111-2222")),
         Instruction(ACTION.RETRIEVE_BY_AREA_CODE, "Y", 609),
-        Instruction(ACTION.WRITE_RECORD,          "X", (2, "Thalia", "412-656-2212")),
+        Instruction(ACTION.WRITE_RECORD,          "X", Record(2, "Thalia", "412-656-2212")),
+        Instruction(ACTION.WRITE_RECORD,          "X", Record(3, "Bin", "412-555-2121")),
         Instruction(ACTION.RETRIEVE_BY_AREA_CODE, "X", 412),
-        Instruction(ACTION.DELETE_TABLE,          "Y"),
+        #Instruction(ACTION.DELETE_TABLE,          "Y"),
         ]
 
-core = Core(disk_org, mem_size, block_size)
-core.run(insts)
+insts2 = [
+        Instruction(ACTION.WRITE_RECORD,          "X", Record(1, "Alex", "412-111-1111")),
+        Instruction(ACTION.WRITE_RECORD,          "X", Record(2, "Bob", "412-222-2222")),
+        Instruction(ACTION.RETRIEVE_BY_ID,        "X", 1),
+        #Instruction(ACTION.DELETE_RECORD,         "Y", 5),
+        Instruction(ACTION.WRITE_RECORD,          "Y", Record(3, "Bin", "412-555-2121")),
+        Instruction(ACTION.DELETE_TABLE,          "Y"),
+        Instruction(ACTION.RETRIEVE_BY_AREA_CODE, "X", 412),
+]
+
+core = Core(disk_org, int(mem_size), int(block_size))
+core.run(real_insts)
