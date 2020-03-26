d48ad81ad968c0114ba8ae05088d630f7e3509ec
Paul Elder <paul.elder@pitt.edu>
2020-03-26 17:01:51 -0400

    scripts: make_writes: add script to stress test writes

---
+++ scripts/make_writes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+for i in range(5000):
+    if i % 2 == 0:
+        phone = "412-888-8888" 
+    else:
+        phone = "412-999-9999" 
+    text = "W tbl1 ("+str(i)+", name"+str(i)+", "+phone+")"
+    print(text)
