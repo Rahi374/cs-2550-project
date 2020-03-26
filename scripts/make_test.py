6b054512ea6c06d93f0fa36d0b7124bd7a667fb2
Paul Elder <paul.elder@pitt.edu>
2020-03-26 17:01:37 -0400

    scripts: make_test: print 5000 operations, to stdout, and remove empty lines

--- scripts/make_test.py
+++ scripts/make_test.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 
-f = open("test_scripts/test_script.txt", "w+")
-text = ""
-for i in range(100):
-    phone = ""
+for i in range(5000):
     if i % 2 == 0:
         phone = "412-888-8888" 
     else:
         phone = "412-999-9999" 
-    text += "W tbl1 ("+str(i)+", name"+str(i)+", "+phone+")\n"
-
+    text = "W tbl1 ("+str(i)+", name"+str(i)+", "+phone+")"
     print(text)
 
-for i in range(100):
-    text += "R tbl1 "+str(i)+"\n"
-
+for i in range(5000):
+    text = "R tbl1 "+str(i)+""
     print(text)
-f.write(text)
-f.close()
-
