e36babdfaa9c2296b79b422cca2e5650626b113c
Paul Elder <paul.elder@pitt.edu>
2020-04-11 20:51:22 -0400

    log_entry: implement log entry class

---
+++ log_entry.py
@@ -0,0 +1,6 @@
+class LogEntry():
+    def __init__(before_image, after_image, inst, t_id):
+        self.before_image = before_image
+        self.after_image = after_image
+        self.inst = inst
+        self.t_id = t_id
