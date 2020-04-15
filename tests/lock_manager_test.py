647cca7dfa0a9ce73c991c1a6547e631e02f5dd9
Paul Elder <paul.elder@pitt.edu>
2020-04-15 19:21:22 -0400

    tests: lock_manager: add test description comments

--- tests/lock_manager_test.py
+++ tests/lock_manager_test.py
@@ -4,28 +4,34 @@ from lock_manager import *
 
 class TestLockManager(unittest.TestCase):
     def test_lock_manager(self):
-        print("Running test of lock manager")
         lock_man = lock_manager()
-        self.assertTrue(lock_man.is_read_lock_available(0,"key0"))
-        self.assertTrue(lock_man.is_read_lock_available(0,"key0"))
-        self.assertFalse(lock_man.is_read_lock_available(1,"key0"))
-        self.assertFalse(lock_man.is_write_lock_available(2,"key0"))
 
-        lock_man.read_lock(0,"key0")
-        lock_man.read_lock(1,"key0")
-        self.assertFalse(lock_man.is_write_lock_available(2,"key0"))
+        # test availability checking and queueing
+        self.assertTrue(lock_man.is_read_lock_available(0, "key0"))
+        self.assertTrue(lock_man.is_read_lock_available(0, "key0"))
+        self.assertFalse(lock_man.is_read_lock_available(1, "key0"))
+        self.assertFalse(lock_man.is_write_lock_available(2, "key0"))
 
+        # test read lock acquiring
+        lock_man.read_lock(0, "key0")
+        lock_man.read_lock(1, "key0")
+        self.assertFalse(lock_man.is_write_lock_available(2, "key0"))
+
+        # test unlocking read lock
         lock_man.unlock_all_locks_for_transaction(0)
         lock_man.unlock_all_locks_for_transaction(1)
-        self.assertTrue(lock_man.is_write_lock_available(2,"key0"))
+        self.assertTrue(lock_man.is_write_lock_available(2, "key0"))
 
+        # test write lock acquiring
         lock_man.write_lock(2, "key0")
-        self.assertFalse(lock_man.is_write_lock_available(1,"key0"))
-        self.assertFalse(lock_man.is_read_lock_available(0,"key0"))
+        self.assertFalse(lock_man.is_write_lock_available(1, "key0"))
+        self.assertFalse(lock_man.is_read_lock_available(0, "key0"))
 
+        # test unlocking write lock
         lock_man.unlock_all_locks_for_transaction(2)
         self.assertTrue(lock_man.is_write_lock_available(1,"key0"))
 
+        # test write lock acquiring again
         lock_man.write_lock(1, "key0")
         self.assertFalse(lock_man.is_write_lock_available(0,"key0"))
     
