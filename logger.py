09b3c8712734cd3fee6598abcc28a885d558d414
Paul Elder <paul.elder@pitt.edu>
2020-04-15 22:28:36 -0400

    logger: log current transaction id

--- logger.py
+++ logger.py
@@ -3,6 +3,7 @@ class Logger(object):
     thread_safe_lock = None
     file_to_write_out = None
     global_text_to_write = None
+    trans_id = None
 
     @staticmethod
     def initialize(file_name):
@@ -13,7 +14,7 @@ class Logger(object):
     @staticmethod
     def log(stringy):
         Logger.thread_safe_lock.acquire()
-        Logger.global_text_to_write += stringy + "\n"
+        Logger.global_text_to_write += ("" if Logger.trans_id == None else str(Logger.trans_id) + " ") +  stringy + "\n"
         Logger.thread_safe_lock.release()
 
     @staticmethod
