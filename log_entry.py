f1c173166d515b850d10e004ef712a3d3903bb86
Paul Elder <paul.elder@pitt.edu>
2020-04-15 22:27:38 -0400

    log_entry: add __str__ and __repr__

--- log_entry.py
+++ log_entry.py
@@ -4,3 +4,9 @@ class LogEntry():
         self.after_image = after_image
         self.inst = inst
         self.t_id = t_id
+
+    def __str__(self):
+        return f"<{self.t_id}, {self.inst}, {self.before_image}, {self.after_image}>"
+
+    def __repr__(self):
+        return f"<{self.t_id}, {self.inst}, {self.before_image}, {self.after_image}>"
