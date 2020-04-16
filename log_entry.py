f0de5e7f7a4f0f96d8a5c21233efc4cf26d56da9
Paul Elder <paul.elder@pitt.edu>
2020-04-15 22:27:25 -0400

    log_entry: fix constructor

--- log_entry.py
+++ log_entry.py
@@ -1,5 +1,5 @@
 class LogEntry():
-    def __init__(before_image, after_image, inst, t_id):
+    def __init__(self, before_image, after_image, inst, t_id):
         self.before_image = before_image
         self.after_image = after_image
         self.inst = inst
